02c3e1f65b9c4ae8907aea7ce1c6abf3b34173f7
aspeziale <aspeziale@digitalidentity.tech>
2018-05-07 12:53:47 +0200

    No more saving my votes in my supporters collection

--- collect.py
+++ collect.py
@@ -1,8 +1,6 @@
-import requests, json, datetime,logging
+import requests, json, datetime, logging, os, time
 from ConfigParser import SafeConfigParser
 from pymongo import MongoClient
-import os
-import time
 
 # parser
 parser = SafeConfigParser()
@@ -20,7 +18,22 @@ logging.basicConfig(format='[%(asctime)s] %(message)s', filename='collect-log.lo
 host = parser.get('Node','protocol')+parser.get('Node','ip')+parser.get('Node','port')
 getvotersendpoint = parser.get('Node','getvotersendpoint')
 pub_key = parser.get('Account','pub_key')
+getvotesendpoint = parser.get('Node','getvotesendpoint')
+address = parser.get('Account','address')
 
+# array of gdt and elite
+gdt = parser.get('Skip', 'gdt')
+elite = gdt = parser.get('Skip', 'elite')
+swap = []
+
+# my votes
+def get_who_i_vote():
+    r = requests.get(host + getvotesendpoint + address)
+    votes = json.loads(r.text)['delegates']
+    global swap 
+    swap = [v['address'] for v in votes]
+
+get_who_i_vote()
 
 def check_time_diff(last_update):
     """
@@ -46,44 +59,45 @@ voters_public_keys = map(lambda y: y['publicKey'], voters)
 voters_already_in_pool.remove({'publicKey':{'$nin':voters_public_keys}})
 
 for v in voters:
-    voter = voters_already_in_pool.find_one({'address': v['address']})
-    if voter:
-        if not check_time_diff(voter['updated_at']):
-            #print 'no update day in pool'
-            db.voters.update(
-                {'address': v['address']},
-                {'$set': {
-                    'updated_at': datetime.datetime.now(),
-                    'balance': v['balance']
-                    }
-                },
-                True)
-            info_str = '{} day in pool not updated'.format(v['username'])
-        else:
-            #print 'update day in pool'
-            db.voters.update(
-                {'address': v['address']},
-                {
-                    '$inc': {'day_in_pool': 1},
-                    '$set': {
+    if not(v['address'] in gdt or v['address'] in elite or v['address'] in swap):
+        voter = voters_already_in_pool.find_one({'address': v['address']})
+        if voter:
+            if not check_time_diff(voter['updated_at']):
+                #print 'no update day in pool'
+                db.voters.update(
+                    {'address': v['address']},
+                    {'$set': {
                         'updated_at': datetime.datetime.now(),
                         'balance': v['balance']
-                    }
+                        }
+                    },
+                    True)
+                info_str = '{} day in pool not updated'.format(v['username'])
+            else:
+                #print 'update day in pool'
+                db.voters.update(
+                    {'address': v['address']},
+                    {
+                        '$inc': {'day_in_pool': 1},
+                        '$set': {
+                            'updated_at': datetime.datetime.now(),
+                            'balance': v['balance']
+                        }
 
-                },
-                True)
-            info_str = '{} day in pool updated'.format(v['username'])
-    else:
-        db.voters.insert_one({
-            'address': v['address'],
-            'day_in_pool': 1,
-            'updated_at': datetime.datetime.now(),
-            'username': v['username'],
-            'publicKey': v['publicKey'],
-            'balance': v['balance']
-        })
-        info_str = '{} welcome'.format(v['username'])
-    logging.info(info_str)
+                    },
+                    True)
+                info_str = '{} day in pool updated'.format(v['username'])
+        else:
+            db.voters.insert_one({
+                'address': v['address'],
+                'day_in_pool': 1,
+                'updated_at': datetime.datetime.now(),
+                'username': v['username'],
+                'publicKey': v['publicKey'],
+                'balance': v['balance']
+            })
+            info_str = '{} welcome'.format(v['username'])
+        logging.info(info_str)
 
 # check if a payout is ever be done, if not add an empty
 collections = db.collection_names()
